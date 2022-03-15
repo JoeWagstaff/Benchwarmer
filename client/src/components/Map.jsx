046a33133c45fc3b1ebfffa5c6d61fed488f14c4
Joe Wagstaff <joe.wagstaff@gmail.com>
2022-03-14 19:00:32 -0700

    started benchwarmer

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,10 @@
+// import { Wrapper, Status } from "@googlemaps/react-wrapper";
+// const key = require('./config.js');
+
+// const Map = (status: Status) => {
+//   return <h1>{status}</h1>;
+// }
+
+// <Wrapper apiKey={key} render={Map}>
+//   <div></div>
+// </Wrapper>
---
+++ client/src/config.js
@@ -0,0 +1,3 @@
+const API_KEY = 'AIzaSyCD7JdkdkLSM8hz66g4f7ApES_h0VA20NQ';
+
+module.exports = API_KEY;
